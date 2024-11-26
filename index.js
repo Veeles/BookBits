399e9596bd1b3f831dc12122bc3132e48c167a6f
Veeles <kacper.ros2@interia.pl>
2024-11-26 19:08:07 +0100

    Add git ignore

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import pg from 'pg';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
-
+let isbn
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+async function getDetails(isbn){
+
+}
 
 app.get('/', (req,res) => {
     res.render('index.ejs')

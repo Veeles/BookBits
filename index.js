89cee16542872724f7f096de6cf854a0eb3c024c
Veeles <kacper.ros2@interia.pl>
2024-11-27 17:39:01 +0100

    Integrate new books submission form with backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pg from 'pg';
 import axios from 'axios';
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
@@ -11,11 +12,12 @@ const user = new pg.Client({
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
-
+user.connect()
 let isbn;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use(express.json());
 
 async function getDetails(isbn){
 
@@ -31,8 +33,45 @@ app.get('/add', (req,res) => {
     res.render('add.ejs')
 });
 
-app.post('/add', (req,res) => {
-    console.log(req.body)
+app.post('/test', async (req,res) => {
+    
+    let data = {
+        name: 'dudu',
+        password: 'tusek'
+    }
+    
+    return await res.json(data)
+});
+
+app.post('/add', async (req,res) => {
+    let returnResult = {
+        success: false,
+    }
+    const isbn = req.body['isbn'];
+    const author = req.body['author'];
+    const title = req.body['title'];
+    try{
+        const result = await axios.get(`https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`);
+        if(result.status == 200){
+            try{
+                const addBook = await user.query('INSERT INTO book (title, author, image) VALUES ($1, $2, $3)', [title, author, `https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`]);
+                returnResult = {
+                    success: true,
+                }
+                res.json(returnResult);
+            } catch (err){
+                res.json(returnResult)
+                console.log(err);
+            };
+        } else{
+            res.json(returnResult)
+            console.log(result.status);
+        };
+    } catch (err){
+        res.json(returnResult)
+        console.log(err);
+    };
+
 });
 
 app.listen(port, () => {
